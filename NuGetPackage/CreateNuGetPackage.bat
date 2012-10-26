83f06f6ec1abdcbe05f473dd4d3757c895c0b4b8
Elijah Frederickson <elijah.frederickson@gmail.com>
2012-10-26 10:12:23 -0800

    Added nuget packaging

---
+++ NuGetPackage/CreateNuGetPackage.bat
@@ -0,0 +1,22 @@
+@echo off
+cd Package
+
+:: create folder
+mkdir lib
+cd lib
+mkdir net40
+cd net40
+
+:: copy binaries
+copy /Y ..\..\..\..\SharpLua\bin\debug\SharpLua.dll .
+
+:: create NuGet package
+cd ..\..
+nuget pack SharpLua.nuspec
+
+move *.nupkg ..
+cd ..
+del Package\lib\net40\SharpLua.dll
+
+for %%f in (*.nupkg) do nuget push "%%f"
+del *.nupkg
